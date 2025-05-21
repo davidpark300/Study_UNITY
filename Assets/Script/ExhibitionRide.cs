bc29d97cf60a58e46406824dae6b8f3a4790b90f
davidpark300 <usjung38@gmail.com>
2025-05-21 17:27:43 +0900

    Create Gallery

---
+++ Assets/Script/ExhibitionRide.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExhibitionRide : MonoBehaviour
+{
+    public GameObject artWorks;
+    public float startDelay = 3f;
+    public float transitionTime = 5f;
+
+    private AnimationCurve xCurve, zCurve, rCurve;
+
+    private void Start()
+    {
+        int count = artWorks.transform.childCount + 1;
+        Keyframe[] xKeys = new Keyframe[count];
+        Keyframe[] zKeys = new Keyframe[count];
+        Keyframe[] rKeys = new Keyframe[count];
+        int i = 0;
+        float time = startDelay;
+        xKeys[0] = new Keyframe(time, transform.position.x);
+        zKeys[0] = new Keyframe(time, transform.position.z);
+        rKeys[0] = new Keyframe(time, transform.rotation.y);
+
+        foreach(Transform artwork in artWorks.transform)
+        {
+            i++;
+            time += transitionTime;
+            Vector3 pos = artwork.position - artwork.forward;
+            xKeys[i] = new Keyframe(time, pos.x);
+            zKeys[i] = new Keyframe(time, pos.z);
+            rKeys[i] = new Keyframe(time, artwork.rotation.y);
+        }
+        xCurve = new AnimationCurve(xKeys);
+        zCurve = new AnimationCurve(zKeys);
+        rCurve = new AnimationCurve(rKeys);
+    }
+
+    private void Update()
+    {
+        transform.position = new Vector3(xCurve.Evaluate(Time.time), transform.position.y, zCurve.Evaluate(Time.time));
+        Quaternion rot = transform.rotation;
+        rot.y = rCurve.Evaluate(Time.time);
+        transform.rotation = rot;
+    }
+
+}
